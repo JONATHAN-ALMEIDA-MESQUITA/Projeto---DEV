6eb7be4f68e46c0d3178d4b9e0829f2957a5a7fe
jonathan mesquita <jonathan.mesquita@hotmail.com>
2022-04-04 19:05:47 -0300

    Dicionario

--- Dicionario.py
+++ Dicionario.py
@@ -4,17 +4,15 @@ dicionario = {
     " Mace Windu": "Mestre jedi",
    "Anajin Skywalker" : "Cavaleiro Jedi",
     "R2-D2":"Droide",
-   "Dex" : "Balconista"
+   "Dex" : "Balconista",
+    "Jonathan" : "Lolzero"
 }
 
 for chave, valor in dicionario.items():
-    print("{} -- {}" .format(chave, valor))
-
-
-
+    print("{} - {}" .format(chave, valor))
 
 dicionario.pop("YODA")
 
 for chave, valor in dicionario.items():
-    print("{} -- {}" .format(chave, valor))
+    print("{} - {}" .format(chave, valor))
 
